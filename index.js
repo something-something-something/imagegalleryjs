fe4e443629db1116d027045b40a9c4ceba431e0f
Eric Krebs <ekrebs@outlook.com>
2019-09-26 17:44:07 -0500

    image modification code

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 'use strict';
 const express=require('express');
 const app=express();
+const fsPromises=require('fs').promises;
+const path=require('path');
+
 
 app.locals.config={
 	galleryDescriptionsDir:'testdata/galleries',
-	galleryItemsDir:'testdata/galleryItems'
+	galleryItemsDir:'testdata/galleryItems',
+	galleryImageDir:'testdata/images',
+	galleryImageJsFunctions:'./testdata/imgjs/img-mod.js'
 };
 
 
+async function modifyimages(jsfile,imgdir,imgpath){
+	let dirconArr=await fsPromises.readdir(path.join(imgdir,imgpath),{
+		withFileTypes:true
+	});
+
+	for (let i of dirconArr){
+		if(i.isDirectory()){
+			modifyimages(jsfile,imgdir,path.join(imgpath,i.name));
+		}
+		else if(i.isFile()){
+			console.log(path.join(imgpath,i.name));
+			if(path.extname(i.name).toLowerCase()==='.png'||path.extname(i.name).toLowerCase()==='.jpeg'||path.extname(i.name).toLowerCase()==='.jpg'){
+				let imageMod=require(jsfile);
+				imageMod(imgdir,path.join(imgpath,i.name));
+			}
+
+
+		}
+	}
+}
+
+modifyimages(app.locals.config.galleryImageJsFunctions,app.locals.config.galleryImageDir,'');
+console.log('hi');
 const galleryRouter=require('./routes/gallery');
 app.get('/',async (req,res)=>{
 	res.send('HOME');
---
+++ testdata/imgjs/img-mod.js
@@ -0,0 +1,93 @@
+const {loadImage,createCanvas}=require('canvas');
+const path=require('path');
+
+const fsPromises=require('fs').promises;
+const fs=require('fs');
+
+async function transformImage(imgdir,imgpath){
+	createImageWithText(imgdir,imgpath);
+	createXheightImage(200,'testdata/smallimg',imgdir,imgpath);
+	createXwidthImage(500,'testdata/medimg',imgdir,imgpath);
+}
+
+
+async function createImageWithText(imgdir,imgpath){
+	let targetdir='testdata/outimg';
+	let img=await loadImage(path.join(imgdir,imgpath));
+	
+	let imgcanvas=createCanvas(img.width,img.height);
+
+	let ctx=imgcanvas.getContext('2d');
+
+	ctx.drawImage(img,0,0);
+	ctx.font='10px serif';
+	ctx.textAlign='end';
+	ctx.fillText('Test img',imgcanvas.width-10,imgcanvas.height-10);
+
+	writeCanvas(targetdir,imgpath,imgcanvas);
+}
+
+async function createXwidthImage(xwidth,targetdir,imgdir,imgpath){
+
+	let img=await loadImage(path.join(imgdir,imgpath));
+	
+	let imgcanvas=createCanvas((xwidth/img.width)*img.width,(xwidth/img.width)*img.height);
+
+	let ctx=imgcanvas.getContext('2d');
+
+	ctx.drawImage(img,0,0,(xwidth/img.width)*img.width,(xwidth/img.width)*img.height);
+	ctx.font='10px serif';
+	ctx.textAlign='end';
+	ctx.fillText('Test img',imgcanvas.width-10,imgcanvas.height-10);
+
+	writeCanvas(targetdir,imgpath,imgcanvas);
+
+	
+}
+
+async function createXheightImage(xheight,targetdir,imgdir,imgpath){
+
+	let img=await loadImage(path.join(imgdir,imgpath));
+	
+	let imgcanvas=createCanvas((xheight/img.height)*img.width,(xheight/img.height)*img.height);
+
+	let ctx=imgcanvas.getContext('2d');
+
+	ctx.drawImage(img,0,0,(xheight/img.height)*img.width,(xheight/img.height)*img.height);
+	ctx.font='10px serif';
+	ctx.textAlign='end';
+	ctx.fillText('Test img',imgcanvas.width-10,imgcanvas.height-10);
+
+	writeCanvas(targetdir,imgpath,imgcanvas);
+
+	
+}
+
+
+async function writeCanvas(targetdir,imgpath,imgcanvas){
+	await fsPromises.mkdir(path.join(targetdir,path.dirname(imgpath)),{
+		recursive:true
+	});
+	let imgwrite=fs.createWriteStream(path.join(targetdir,imgpath));
+
+	let imgStream;
+	if(path.extname(imgpath).toLowerCase()==='.jpg'||path.extname(imgpath).toLowerCase()==='.jpeg'){
+		console.log('jpg');
+		imgStream=imgcanvas.createJPEGStream();
+	}
+	else if(path.extname(imgpath).toLowerCase()==='.png'){
+		console.log('png');
+		imgStream=imgcanvas.createPNGStream();
+	}
+	else{
+		console.log('none');
+		return;
+	}
+	imgStream.pipe(imgwrite);
+	imgwrite.on('finish',()=>{
+		console.log('done')
+	});
+}
+
+
+module.exports =transformImage;
